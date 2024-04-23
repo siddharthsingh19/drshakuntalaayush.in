4c55c6a00e19c4a6940f6849ee1ef8002e53062e
Siddharth Singh <siddharth.singh19@outlook.com>
2024-04-23 16:23:44 +0530

    about dropdown

--- src/components/Navbar/navdrops/Aboutdrop.jsx
+++ src/components/Navbar/navdrops/Aboutdrop.jsx
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import collegecouncil from "/college council.pdf";
 import "./aboutdrop.css";
+import { FaCaretRight } from "react-icons/fa";
+import statenoc from "/statenoc.jpg";
+import affil from "/Affiliation_Letter_2021.jpg";
+import permission from "/Permission2024.jpg";
 const Aboutdrop = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenIn, setIsOpenIn] = useState(false);
 
   const handleMouseEnter = () => {
     setIsOpen(true);
@@ -14,12 +19,15 @@ const Aboutdrop = () => {
   };
 
   return (
-    <>
-      <span className="aboutdrop--main" onMouseEnter={handleMouseEnter}>
+    <div className="dropdown-navigation">
+      <div
+        className="dropdown-navigation aboutdrop--main"
+        onMouseEnter={handleMouseEnter}
+      >
         About Us
-      </span>
+      </div>
       {isOpen && (
-        <div onMouseLeave={handleMouseLeave}>
+        <div className="dropdown-menu" onMouseLeave={handleMouseLeave}>
           <div className="aboutdrop--links">
             <a target="_blank" href={collegecouncil}>
               College Council
@@ -40,15 +48,42 @@ const Aboutdrop = () => {
           <div className="aboutdrop--links">
             <Link to="/rules&regulations">Rules & Regulation</Link>
           </div>
-          <div className="aboutdrop--links">
-            <Link to="/permission&affiliation">Permission & Affiliation</Link>
+
+          <div
+            onMouseEnter={handleMouseEnter}
+            className="aboutdrop--links dropdown-navigationIn"
+          >
+            <Link to="/permission&affiliation" className="flex aic jcsb">
+              Permission & Affiliation <FaCaretRight />
+            </Link>
+            {isOpen && (
+              <div className="dropdown-menuIn" onMouseLeave={handleMouseLeave}>
+                <div className="aboutdrop--links">
+                  <a target="_blank" href={statenoc}>
+                    State NOC
+                  </a>
+                </div>
+                <div className="aboutdrop--links">
+                  <a target="_blank" href={affil}>
+                    University Affiliation
+                  </a>
+                </div>
+                <div className="aboutdrop--links">
+                  <a target="_blank" href={permission}>
+                    Permission Letter by Goverment of India
+                  </a>
+                </div>
+              </div>
+            )}
           </div>
           <div className="aboutdrop--links">
-            <Link to="/otheractivites">Other Activites</Link>
+            <Link to="/otheractivites" className="flex aic jcsb">
+              Other Activites <FaCaretRight />
+            </Link>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
