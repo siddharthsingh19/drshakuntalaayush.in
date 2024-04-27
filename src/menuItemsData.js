36f97b56ce1d7788c3447ffad6e95e3c5d43d0d4
Siddharth Singh <siddharth.singh19@outlook.com>
2024-04-27 17:55:25 +0530

    new navbar incoming

---
+++ src/menuItemsData.js
@@ -0,0 +1,233 @@
+export const menuItemsData = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/about",
+    submenu: [
+      {
+        title: "About College",
+        url: "about-college",
+      },
+      {
+        title: "Mission & Vision",
+        url: "Mission-Vision",
+        // submenu: [
+        //   {
+        //     title: "Frontend",
+        //     url: "frontend",
+        //   },
+        //   {
+        //     title: "Backend",
+        //     submenu: [
+        //       {
+        //         title: "NodeJS",
+        //         url: "node",
+        //       },
+        //       {
+        //         title: "PHP",
+        //         url: "php",
+        //       },
+        //     ],
+        //   },
+        // ],
+      },
+      {
+        title: "Chairman's Message",
+        url: "chairman",
+      },
+      {
+        title: "Director's Message",
+        url: "director",
+      },
+      {
+        title: "Principal's Message",
+        url: "principal",
+      },
+      {
+        title: "Awards & Achievements",
+        url: "Awards",
+      },
+    ],
+  },
+  {
+    title: "Student",
+    url: "/student",
+    submenu: [
+      {
+        title: "Courses & Fees",
+        url: "courses-and-fees",
+      },
+
+      {
+        title: "Admission Inquiry",
+        url: "admission-inquiry",
+      },
+      {
+        title: "Student List",
+        url: "student-list",
+      },
+      {
+        title: "E-Brochure",
+        url: "e-brochure",
+      },
+      {
+        title: "Result",
+        url: "result",
+        submenu: [
+          {
+            title: "BAMS (UG) Result",
+            url: "bams-result",
+          },
+          {
+            title: "PG Medical AU Result",
+            submenu: [
+              {
+                title: "MD First Year",
+                url: "md1styear",
+              },
+              {
+                title: "MD Final Year",
+                url: "mdFinalYear",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "NCISM Mandate",
+    url: "/ncism",
+    submenu: [
+      {
+        title: "Principal and Medical Superintendent",
+        url: "principal-and-medical-superintendent",
+      },
+
+      {
+        title: "Teaching Staff",
+        url: "teaching-staff",
+      },
+      {
+        title: "Intake Capacity",
+        url: "intake-capacity",
+      },
+      {
+        title: "Non Teaching Staff",
+        url: "non-teaching-staff",
+      },
+      {
+        title: "Student Details",
+        url: "student-details",
+      },
+      {
+        title: "Research Publication",
+        url: "research-publication",
+      },
+      {
+        title: "Conferences and Academic Activities",
+        url: "conferences-and-academic-activities",
+      },
+      {
+        title: "Attendance Report",
+        url: "attendance-report",
+        submenu: [
+          {
+            title: "BAMS Students Attendance",
+            url: "bams-students-attendance",
+          },
+          {
+            title: "Hospital Non-Teaching Staff Attendance",
+            url: "hospital-non-teaching-staff-attendance",
+          },
+          {
+            title: "College Non-Teaching Staff Attendance",
+            url: "college-non-teaching-staff-attendance",
+          },
+          {
+            title: "Teaching Staff Attendance",
+            url: "teaching-staff-attendance",
+          },
+        ],
+      },
+      {
+        title: "Affiliations",
+        url: "affiliations",
+      },
+      {
+        title: "Clinical Material in Hospital",
+        url: "clinical-material-in-hospital",
+      },
+      {
+        title: "Hospital Month wise OP/IP Statistics",
+        url: "hospital-month-wise-opip-statistics",
+      },
+    ],
+  },
+  {
+    title: "Facilities",
+    url: "/facilities",
+    submenu: [
+      {
+        title: "Hospital Facilities",
+        url: "hospital-facilities",
+      },
+      {
+        title: "College & Other Facilities",
+        url: "college-and-other-facilities",
+      },
+      {
+        title: "Hostel",
+        url: "hostel",
+      },
+      {
+        title: "Gym",
+        url: "gym",
+      },
+    ],
+  },
+  {
+    title: "Media",
+    url: "/media",
+    submenu: [
+      {
+        title: "Image Gallery",
+        url: "image-gallery",
+      },
+      {
+        title: "Video Gallery",
+        url: "video-gallery",
+      },
+      {
+        title: "Events",
+        url: "events",
+        submenu: [
+          {
+            title: "Conference and Academic Activities",
+            url: "conference-and-academic-activities",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Hospital Clinical Data",
+    url: "/hospital-clinical-data",
+    submenu: [
+      {
+        title: "Hospital OPD/IPD Data",
+        url: "hospital-opd-ipd-data",
+      },
+      {
+        title: "Other Hospital Data",
+        url: "other-hospital-data",
+      },
+    ],
+  },  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
