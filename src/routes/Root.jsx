e07c29ee367ef315244f35f1ddc849aff62934dd
Siddharth Singh <siddharth.singh19@outlook.com>
2024-05-08 22:32:44 +0530

    c

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-
+import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer/Footer";
 import Top from "../components/Top";
